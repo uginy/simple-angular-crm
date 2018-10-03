c386f60550aab73cad728dcc246802d65aa94be7
Ugin <ardisukr@gmail.com>
2018-10-04 01:25:30 +0300

    Initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
+
+import {AppComponent} from './app.component'
+import {LoginPageComponent} from './login-page/login-page.component'
+import {AppRoutingModule} from './app-routing.module'
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component'
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component'
+import {RegisterPageComponent} from './register-page/register-page.component'
+import {TokenInterceptor} from './shared/classes/token.interceptor';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterPageComponent,
+    OverviewPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: TokenInterceptor
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //mongoURI: 'mongodb://crmuser:Crm123@ds115353.mlab.com:15353/simple-crm',
+    mongoURI: 'mongodb://root@localhost/simplecrm',
+  jwt: 'dev-jwt'
+}
